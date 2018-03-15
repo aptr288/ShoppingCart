b78071ad3c0738a73a356b360ddb7496598f84fc
unknown <aptr288@gmail.com>
2018-03-14 22:50:24 -0500

    Updating CART with quantity and total

--- addtocart.php
+++ addtocart.php
@@ -11,14 +11,12 @@ if(isset($_GET['id']) & !empty($_GET['id'])){
 			$quant = 1;
 		}
 		$_SESSION['cart'][$id]= array('quantity' => $quant);
+		header('location: cart.php');
 
 }
 else
 {
 	header('location: cart.php');
 }
-echo "<pre>";
-print_r($_SESSION['cart']);
-echo "</pre>";
 
-?>
+
--- cart.php
+++ cart.php
@@ -1,6 +1,12 @@
+<?php 
+session_start();
+require_once 'config/connect.php';
+include 'inc/header.php'; 
+include 'inc/nav.php'; 
+//session holds the cart values like Id and their quantity 
+$cart = $_SESSION['cart'];
+?>
 
-<?php include 'inc/header.php' ?>
-<?php include 'inc/nav.php' ?>
 	
 	<!-- SHOP CONTENT -->
 	<section id="content">
@@ -25,58 +31,57 @@
 					</tr>
 				</thead>
 				<tbody>
+				<?php
+					
+				$total = 0;
+				//Here we access the cart id and variable array
+				//Iterate through for each loop and give id as the key of the array that is again the Id of each element in array
+					foreach ($cart as $key => $value) {
+			
+						$cartsql = "SELECT * FROM products WHERE id=$key";
+						$cartres = mysqli_query($connection, $cartsql);
+						$cartr = mysqli_fetch_assoc($cartres);
+
+					
+				 ?>
 					<tr>
 						<td>
-							<a class="remove"><i class="fa fa-times"></i></a>
+							<a class="remove" href="delcart.php?id=<?php echo $key; ?>"><i class="fa fa-times"></i></a>
 						</td>
 						<td>
-							<a href="#"><img src="images/shop/1.jpg" alt="" height="90" width="90"></a>					
+							<a href="#"><img src="admin/<?php echo $cartr['thumb']; ?>" alt="" height="90" width="90"></a>					
 						</td>
 						<td>
-							<a href="#">Shaving Knives</a>					
+							<!--The product name is limited to 30 charecters-->
+							<a href="single.php?id=<?php echo $cartr['id']; ?>"><?php echo substr($cartr['name'], 0 , 30); ?></a>					
 						</td>
 						<td>
-							<span class="amount">£69.99</span>					
+							<span class="amount">INR<?php echo $cartr['price']; ?>.00/-</span>					
 						</td>
 						<td>
-							<div class="quantity">1</div>
+							<div class="quantity"><?php echo $value['quantity']; ?></div>
 						</td>
 						<td>
-							<span class="amount">£69.99</span>					
+							<!--Here the amount is calculated by multplying the quantity with the price -->
+							<span class="amount">INR<?php echo ($cartr['price']*$value['quantity']); ?>.00/-</span>					
 						</td>
 					</tr>
-					<tr>
-						<td>
-							<a class="remove"><i class="fa fa-times"></i></a>
-						</td>
-						<td>
-							<a href="#"><img src="images/shop/2.jpg" alt="" height="90" width="90"></a>					
-						</td>
-						<td>
-							<a href="#">Comb Scissors</a>					
-						</td>
-						<td>
-							<span class="amount">£119.99</span>					
-						</td>
-						<td>
-							<div class="quantity">1</div>
-						</td>
-						<td>
-							<span class="amount">£119.99</span>					
-						</td>
-					</tr>
+				<?php 
+				// final total is also calculated by summing the total for each product 
+					$total = $total + ($cartr['price']*$value['quantity']);
+				} ?>
 					<tr>
 						<td colspan="6" class="actions">
 							<div class="col-md-6">
-								<div class="coupon">
+							<!--	<div class="coupon">
 									<label>Coupon:</label><br>
 									<input placeholder="Coupon code" type="text"> <button type="submit">Apply</button>
-								</div>
+								</div> -->
 							</div>
 							<div class="col-md-6">
 								<div class="cart-btn">
-									<button class="button btn-md" type="submit">Update Cart</button>
-									<button class="button btn-md" type="submit">Checkout</button>
+									<!-- <button class="button btn-md" type="submit">Update Cart</button> -->
+									<a href="checkout.php" class="button btn-md" >Checkout</a>
 								</div>
 							</div>
 						</td>
@@ -91,7 +96,7 @@
 						<tbody>
 							<tr>
 								<th>Cart Subtotal</th>
-								<td><span class="amount">£190.00</span></td>
+								<td><span class="amount">INR <?php echo $total; ?>.00/-</span></td>
 							</tr>
 							<tr>
 								<th>Shipping and Handling</th>
@@ -101,7 +106,7 @@
 							</tr>
 							<tr>
 								<th>Order Total</th>
-								<td><strong><span class="amount">£190.00</span></strong> </td>
+								<td><strong><span class="amount">INR <?php echo $total; ?>.00/-</span></strong> </td>
 							</tr>
 						</tbody>
 					</table>
@@ -113,6 +118,4 @@
 			</div>
 		</div>
 	</section>
-	
-	<div class="clearfix space70"></div>
 <?php include 'inc/footer.php' ?>
